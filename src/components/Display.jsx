89bd29047039a8862e2476041b31d0814351d563
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-05 16:32:39 -0800

    Display skeleton written

---
+++ src/components/Display.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Input from './Input';
+
+const Display = ({ received, handleInput, handleClick }) => (
+  <div>
+    <Input
+      handleInput={handleInput}
+      handleClick={handleClick}
+    />
+    <p>{received}</p>
+  </div>
+);
+
+Display.propTypes = {
+  received: PropTypes.string.isRequired,
+  handleInput: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired
+};
+
+export default Display;

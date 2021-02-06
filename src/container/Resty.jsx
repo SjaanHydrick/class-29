84d0bbbc5d6acebebde95647c1ec4883dff45f5c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-05 16:57:52 -0800

    Resty skeleton written

---
+++ src/container/Resty.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Display from '../components/Display';
+import {
+  fetchPost,
+  createPost,
+  updatePost,
+  deletePost
+} from '../services/fetches';
+
+export default class Resty extends Component {
+    state = {
+      url: '',
+      method: '',
+      json: {},
+      received: ''
+    }
+
+    handleInput = ({ target }) => {
+      this.setState({
+        [target.name]: target.value
+      });
+    }
+
+    handleClick = async(e) => {
+      e.preventDefault();
+      const { url, method, json } = this.state;
+
+      if(method === 'get') {
+        const received = await fetchPost(url);
+        await this.setState({
+          received: JSON.stringify(received)
+        });
+      }
+
+      if(method === 'post') {
+        const received = await createPost(url, JSON.parse(json));
+        await this.setState({
+          received: JSON.stringify(received)
+        });
+      }
+
+      if(method === 'put') {
+        const received = await updatePost(url, JSON.parse(json));
+        await this.setState({
+          received: JSON.stringify(received)
+        });
+      }
+
+      if(method === 'delete') {
+        const received = await deletePost(url, JSON.parse(json));
+        await this.setState({
+          received: JSON.stringify(received)
+        });
+      }
+    }
+
+    render() {
+      const { received } = this.state;
+      return (
+        <div>
+          <Display
+            received={received}
+            handleClick={this.handleClick}
+            handleInput={this.handleInput}
+          />
+        </div>
+      );
+    }
+}

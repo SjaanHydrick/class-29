d349103ba64b4b65493b5537c63d0078cb678162
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-07 13:36:39 -0800

    posts test written and passing

--- src/container/Resty.test.jsx
+++ src/container/Resty.test.jsx
@@ -26,4 +26,31 @@ describe('CRUD routes', () => {
       expect(display).toHaveTextContent('"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit"');
     });
   });
+
+  it('posts a new item', () => {
+    render(<Resty />);
+      
+    const display = screen.getByTestId('display');
+    const url = screen.getByTestId('url');
+    const method = screen.getByLabelText('POST');
+    const json = screen.getByPlaceholderText('POST/PUT Request...');
+    const submit = screen.getByText('Submit');
+    fireEvent.change(url, {
+      target: {
+        value: 'https://jsonplaceholder.typicode.com/posts'
+      }
+    }); 
+    fireEvent.change(json, {
+      target: {
+        value: '{"userId":2,"title":"asdf","body":"asdff"}'
+      }
+    });
+
+    fireEvent.click(method);
+    fireEvent.submit(submit);
+
+    return waitFor(() => {
+      expect(display).toHaveTextContent('"userId":2, "title":"asdf", "body":"asdff", "id":101');
+    });
+  });
 });
